f2a8dbd8c02e9cd2396b522797920ec19e9b0098
Gustavo Guichard <gustavoguichard@gmail.com>
2020-09-12 23:50:23 -0300

    💅

--- components/main-search.js
+++ components/main-search.js
@@ -31,25 +31,27 @@ export default function MainSearch({ asanas, tags }) {
   return (
     <div className="flex flex-col py-6 px-8 mb-12 bg-gray-200 border-2 border-gray-300 rounded-lg">
       <form className="flex flex-col" onSubmit={handleSubmit}>
-        <h3 className="text-center">Asana</h3>
+        <h2 className="mb-1 text-xl text-center leading-snug">
+          Comece buscando um <em>Asana</em> ou <mb-1 em>Pranayama</mb-1>
+        </h2>
         <select {...select('asana')} className="w-full p-4 text-lg mt-2">
           <SelectOptions options={asanas} prefix="asana" />
         </select>
         <div className="flex flex-wrap justify-center mt-6">
           <div className="flex flex-col items-center">
-            <span>Condição</span>
+            <span className="text-md">Condição</span>
             <select {...select('health')} className="p-4 text-lg my-1 mx-2">
               <SelectOptions options={health} prefix="health" />
             </select>
           </div>
           <div className="flex flex-col items-center">
-            <span>Nível</span>
+            <span className="text-md">Nível</span>
             <select {...select('level')} className="p-4 text-lg my-1 mx-2">
               <SelectOptions options={level} prefix="level" />
             </select>
           </div>
           <div className="flex flex-col items-center">
-            <span>Prop</span>
+            <span className="text-md">Prop</span>
             <select {...select('props')} className="p-4 text-lg my-1 mx-2">
               <SelectOptions options={props} prefix="props" />
             </select>
