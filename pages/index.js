fc46792747ae2d5a7822c275924fb8d4fe8b11a4
Gustavo Guichard <gustavoguichard@gmail.com>
2020-09-13 01:29:16 -0300

    Update ManifestO

--- pages/index.js
+++ pages/index.js
@@ -28,6 +28,33 @@ export default function Home({ asanas, tags }) {
             enriquece' não apenas deve ser compreendida por nós, mas também
             praticada.
           </blockquote>
+          <p>
+            Com isso em mente, resolvemos fazer esta plataforma que tem o
+            objetivo de ser um material complementar de estudos, também como uma
+            forma de democratizar o conhecimento do método.
+          </p>
+          <p>
+            Como estudantes, praticantes e/ou professores do método{' '}
+            <em>Iyengar</em>, temos muitas anotações de cursos, aulas, workshops
+            e de nossas práticas pessoais. Para ter a possibilidade de pesquisar
+            esse material rapidamente, resolvemos desenvolver uma ferramenta que
+            nos ajude.
+          </p>
+          <p>
+            Também queremos que esse conhecimento acessível e compartilhável, o
+            que tende a ajudar na evolução do método sob uma perspectiva
+            científica. Guruji encarava o Yoga como uma ciência em constante
+            desenvolvimento:
+          </p>
+          <blockquote>Que meu fim seja o seu começo</blockquote>
+          <p>
+            <strong>Importante:</strong> O conteúdo descrito nesta plataforma
+            deve ser usado como material de apoio. Ele não substitui a
+            orientação profissional e/ou as aulas presenciais. Também não
+            pretende ser tomado como correto já que é altamente embasado em
+            nossas próprias práticas pessoais.
+          </p>
+          <p className="text-lg text-center">Namastê</p>
         </main>
       </div>
     </Layout>
