0a55acc2a74c3dcf891eb33036508f43951ac3de
Gustavo Guichard <gustavoguichard@gmail.com>
2020-09-12 22:07:11 -0300

    💅

--- components/main-content.js
+++ components/main-content.js
@@ -8,11 +8,7 @@ export default function PostureMainContent({
     <div>
       <div className="md:flex justify-between items-start">
         <div className="md:order-2 md:w-5/12 mb-3 block bg-gray-200 border-gray-300 border-2 rounded overflow-hidden">
-          <img
-            className="max-w-full m-0 border-2 border-gray-500"
-            src={picture}
-            alt="Trikonasana"
-          />
+          <img className="max-w-full m-0" src={picture} alt="Trikonasana" />
           {cardContent}
         </div>
         <main className="md:w-7/12 md:mr-8">{children}</main>
