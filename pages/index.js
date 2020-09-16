acbcb276e8a575aa179294dfbc027eb8841f524b
Gustavo Guichard <gustavoguichard@gmail.com>
2020-09-16 17:03:14 -0300

    Update copy

--- pages/index.js
+++ pages/index.js
@@ -9,37 +9,23 @@ export default function Home({ asanas, tags }) {
       <div className="md:flex mt-4 md:mt-8 flex-col items-center">
         <MainSearch asanas={asanas} tags={tags} />
         <main className="md:w-7/12 md:ml-8">
-          <h1 className="text-center mb-6">Manifesto</h1>
-          <p className="text-center">
-            Quando perguntaram ao Guruji o porquê dele não registrar patente dos
-            props ele disse:
+          <p>
+            Somos estudantes, praticantes e/ou professores do método{' '}
+            <em>Iyengar Yoga</em>, temos muitas anotações de cursos, aulas,
+            workshops e de nossas práticas pessoais. Para ter a possibilidade de
+            pesquisar esse material rapidamente, resolvemos desenvolver uma
+            ferramenta que nos ajude.
           </p>
-          <blockquote>
-            Eu desenvolvi os props para que as pessoas possam se beneficiar.
-            Milhares estão se beneficiando e continuarão se beneficiando deles.
-            Deus alguma vez registrou uma patente para sua criação? Então que
-            direito eu, um mero mortal, tenho para fazer isso?
-          </blockquote>
-          <p className="text-center">
-            Também sabemos que Geetaji um dia falou:
+          <p>
+            Com isso em mente, resolvemos fazer esta plataforma que tem o
+            objetivo de servir de material complementar dos estudos e como uma
+            forma de democratizar o conhecimento do método.
           </p>
           <blockquote>
             A afirmação de Guruji 'dar não nos empobrece, reter não nos
             enriquece' não apenas deve ser compreendida por nós, mas também
-            praticada.
+            praticada. <em>- Geetaji</em>
           </blockquote>
-          <p>
-            Com isso em mente, resolvemos fazer esta plataforma que tem o
-            objetivo de servir de material complementar dos estudos e como uma
-            forma de democratizar o conhecimento do método <em>Iyengar Yoga</em>
-            .
-          </p>
-          <p>
-            Como estudantes, praticantes e/ou professores do método, temos
-            muitas anotações de cursos, aulas, workshops e de nossas práticas
-            pessoais. Para ter a possibilidade de pesquisar esse material
-            rapidamente, resolvemos desenvolver uma ferramenta que nos ajude.
-          </p>
           <p>
             Também queremos que esse conhecimento seja acessível e
             compartilhável, o que tende a ajudar na evolução do método sob uma
@@ -54,6 +40,16 @@ export default function Home({ asanas, tags }) {
             pretende ser tomado como correto já que é altamente embasado em
             nossas próprias práticas pessoais.
           </p>
+          <p className="text-center">
+            Quando perguntaram ao Guruji o porquê dele não registrar patente dos
+            props ele disse:
+          </p>
+          <blockquote>
+            Eu desenvolvi os props para que as pessoas possam se beneficiar.
+            Milhares estão se beneficiando e continuarão se beneficiando deles.
+            Deus alguma vez registrou uma patente para sua criação? Então que
+            direito eu, um mero mortal, tenho para fazer isso?
+          </blockquote>
           <p className="text-lg text-center">Namastê</p>
         </main>
       </div>
