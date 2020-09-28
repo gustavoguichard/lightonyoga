1d6d76c0be2a2356c847d1697013890314872ea9
Gustavo Guichard <gustavoguichard@gmail.com>
2020-09-28 14:12:35 -0300

    Adjustments on search page

--- pages/search.js
+++ pages/search.js
@@ -10,21 +10,30 @@ import Tag from 'components/tag'
 import AsanaList from 'components/asana-list'
 import VariationsList from 'components/variations-list'
 
-const changedSearch = (allAsanas, allVariations, allTags, setLoading, setList, setSearch) => async (query) => {
+const changedSearch = (
+  allAsanas,
+  allVariations,
+  allTags,
+  setLoading,
+  setList,
+  setSearch,
+) => async (query) => {
   setLoading(true)
   const asanasIds = query.asanas?.split(',').map((n) => Number(n)) || []
   const tagsIds = query.tags?.split(',').map((n) => Number(n)) || []
-  const asanas = allAsanas.filter(a => asanasIds.includes(a.id))
+  const asanas = allAsanas.filter((a) => asanasIds.includes(a.id))
   const tags = allTags.filter((t) => tagsIds.includes(t.id))
-  const variations = allVariations.filter((v) => intersection(tagsIds, v.tag_ids).length)
+  const variations = allVariations.filter(
+    (v) => intersection(tagsIds, v.tag_ids).length,
+  )
   setSearch({ asanas, tags })
   const list = asanas.length
-  ? asanas.reduce((obj, asana) => {
-    const name = `Variações de ${asana.name}`
-    obj[name] = variations.filter(v => v.asana.id === asana.id)
-    return obj
-  }, {})
-  : { Variações: variations }
+    ? asanas.reduce((obj, asana) => {
+        const name = `Variações de ${asana.name}`
+        obj[name] = variations.filter((v) => v.asana.id === asana.id)
+        return obj
+      }, {})
+    : { Variações: variations }
   setList(list)
   setLoading(false)
 }
@@ -33,7 +42,14 @@ export default function Search({ asanas, variations, tags }) {
   const [loading, setLoading] = useState(true)
   const [list, setList] = useState([])
   const [search, setSearch] = useState({})
-  const handleChange = changedSearch(asanas, variations, tags, setLoading, setList, setSearch)
+  const handleChange = changedSearch(
+    asanas,
+    variations,
+    tags,
+    setLoading,
+    setList,
+    setSearch,
+  )
 
   const router = useRouter()
 
@@ -42,20 +58,24 @@ export default function Search({ asanas, variations, tags }) {
   }, [router.query])
 
   return (
-    <Layout title="Busca">
+    <Layout
+      title="Resultados da busca"
+      subtitle={
+        !!search.tags?.length ? (
+          <span>
+            Filtros:{' '}
+            {search.tags?.map((tag) => (
+              <Tag key={tag.name + tag.id} tag={tag} />
+            ))}
+          </span>
+        ) : null
+      }
+    >
       {loading ? (
         'Carregando...'
       ) : (
         <div className="md:flex md:flex-col">
-          <div>
-            <h4>Resultados para:</h4>
-            {search.tags?.map((tag) => (
-              <Tag key={tag.name + tag.id} tag={tag} />
-            ))}
-            {!!search.asanas?.length && (
-              <AsanaList hideTitle asanas={search.asanas} />
-            )}
-          </div>
+          {!!search.asanas?.length && <AsanaList asanas={search.asanas} />}
           {!!search.tags?.length &&
             map(list, (variations, name) => (
               <div className="w-full" key={name}>
