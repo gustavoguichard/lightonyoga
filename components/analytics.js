4efcd4ddb2f30b6d7a4ca0278a4b3b7b66cd774f
Gustavo Guichard <gustavoguichard@gmail.com>
2021-01-24 17:22:49 -0300

    Add Analytics

---
+++ components/analytics.js
@@ -0,0 +1,20 @@
+export default function Analytics() {
+  return (
+    <>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-DRD2D8ZVS0"
+      ></script>
+      <script
+        async
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', process.env.NEXT_PUBLIC_ANALYTICS_ID);`,
+        }}
+      />
+    </>
+  )
+}
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Analytics from 'components/analytics'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -14,6 +15,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.xz.style/serve/inter.css"
           />
+          <Analytics />
         </Head>
         <body>
           <Main />
